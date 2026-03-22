7d9671947e60581da8848f2e1fb3dec8d71370bc
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-03-22 21:58:32 +0500

    Refactor App component structure and enhance styling in index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Logo />
+      <Form />
+      <ItemsList />
+      <Stats />
+    </>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>Far away</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>what do you need for your trip?</h3>
+    </div>
+  );
+}
+function ItemsList() {
+  return <div className="list">List</div>
+}
+function Stats() {
+  return <footer>You have X items on your list, and you already packed X items (X%)</footer>
+}
