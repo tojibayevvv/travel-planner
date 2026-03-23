1e5d31c5bec1e3e82c4bf72519fa0806d33dcea1
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-03-23 14:13:28 +0500

    on proccess

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 2, packed: true },
+];
 
 export default function App() {
   return (
@@ -15,15 +21,69 @@ function Logo() {
   return <h1>Far away</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 30 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
   );
 }
 function ItemsList() {
-  return <div className="list">List</div>
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : null}>
+        {item.quantity} {item.description}
+      </span>
+    </li>
+  );
+}
+
 function Stats() {
-  return <footer>You have X items on your list, and you already packed X items (X%)</footer>
+  return (
+    <footer className="stats">
+      <em>
+        You have X items on your list, and you already packed X items (X%)
+      </em>
+    </footer>
+  );
 }
