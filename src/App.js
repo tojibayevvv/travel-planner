ce664dfd9e7cf25ae94d7da3b13e247cc1520438
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-03-25 17:25:02 +0500

    worked with form

--- src/App.js
+++ src/App.js
@@ -23,12 +23,20 @@ function Logo() {
 function Form() {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
 
+    if (!description) return;
+
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+    handleAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
