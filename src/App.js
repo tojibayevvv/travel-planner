d9917d6c9a99242fdc2be6bdd842010ed4d200d2
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-03-29 17:26:57 +0500

    enhance stats display with empty state and improved packing message

--- src/App.js
+++ src/App.js
@@ -114,14 +114,21 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Let's pack stuff for your trip 🚀</em>
+      </p>
+    );
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
       <em>
-        You have {numItems} items on your list, and you already packed{" "}
-        {numPacked} items ({percentage}%)
+        {percentage === 100
+          ? "You packed everything! You are ready to go 🛫"
+          : `You have ${numItems} items on your list, and you already packed ${numPacked} items (${percentage}%)`}
       </em>
     </footer>
   );
